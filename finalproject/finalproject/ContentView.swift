72321232d49092a97ced0735e5ea2b1a76f00a4a
Moneerahalajmi <moneeraharyam@gmail.com>
2022-08-28 18:33:01 +0300

    final

---
+++ finalproject/finalproject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  finalproject
+//
+//  Created by Moneerah Alajmi on 8/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ finalproject/finalproject/finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectApp.swift
+//  finalproject
+//
+//  Created by Moneerah Alajmi on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
